a5c15eeb422fe4b82ee4af3141f69f15ef8b8102
Chris Fryer <c.j.fryer@lse.ac.uk>
2016-10-25 10:58:30 +0100

    Reimplement 216cc6a and 306be8c

--- simple-download-monitor/includes/sdm-utility-functions.php
+++ simple-download-monitor/includes/sdm-utility-functions.php
@@ -108,7 +108,11 @@ function sdm_ip_info($ip, $purpose = "location") {
         "SA" => "South America"
     );
 
-    $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+    $res = wp_safe_remote_get("http://www.geoplugin.net/json.gp?ip=" . $ip);
+    if ( is_wp_error($res) ) {
+        return null;
+    }
+    $ipdat = @json_decode($res['body']);
 
     if (@strlen(trim($ipdat->geoplugin_countryCode)) === 2) {
         switch ($purpose) {
